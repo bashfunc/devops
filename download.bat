54c64a0d26e00ed0cd940e042136e666b3c3a6fd
tom <tom@sapletta.com>
2021-01-17 12:28:13 +0100

    update order structure

---
+++ download.bat
@@ -0,0 +1,8 @@
+cd ..
+git clone https://github.com/bashfunc/logo
+git clone https://github.com/bashfunc/docs
+git clone https://github.com/bashfunc/www
+git clone https://github.com/bashfunc/git
+git clone https://github.com/bashfunc/json-parser
+git clone https://github.com/bashfunc/http-test
+git clone https://github.com/bashfunc/docker
---
+++ remove.bat
@@ -0,0 +1,8 @@
+cd ..
+rm -rf logo
+rm -rf docs
+rm -rf www
+rm -rf git
+rm -rf json-parser
+rm -rf http-test
+rm -rf docker
